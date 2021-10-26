3da7c8033046736ed7bf981389eb582d34064d3e
Amelia GA <ameliaGA11112@gmail.com>
2021-10-26 04:52:24 -0500

    hice modificacion de la pagina principal ya teniendo datos originales de la informacion que recabe, tambien integre modal de libreria materialize aun sin datos pero funciona mas input ya acomodado, practicamente estructura deacuerdo al wireframe aun sin colores originales

--- assets/js/data.js
+++ assets/js/data.js
@@ -57,6 +57,32 @@ function valor(value){
     $('.carousel').carousel();
   });
 
+  //inicializacions de modal
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+
+   //var instance = M.Modal.getInstance(elem);
+
+  /* jQuery Method Calls
+    You can still use the old jQuery plugin method calls.
+    But you won't be able to access instance properties.
+
+    $('.modal').modal('methodName');
+    $('.modal').modal('methodName', paramName);
+  */
+      //instance.close();
+
+  //continuacion de mi codigo html
+
 let input=document.getElementById('searchInput');
 let lista=document.getElementById('list');
 input.addEventListener('keyup',()=> typing(dataValue))
