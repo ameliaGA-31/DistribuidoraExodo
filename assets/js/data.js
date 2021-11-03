6f719e80843783ec1dfc7791d52f31ad439cd6b8
Amelia GA <ameliaGA11112@gmail.com>
2021-11-03 12:44:46 -0600

    agregand contenido a los selectores y evento a opciones

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,6 +1,6 @@
 const spreadsheetsId='1WfxVETEfl-fmU4QGfOG1wk-93ClARjK5wkgUbAg3BXg';
 //const range='Respuestas de formulario 1!A1:F5';
-const range='Data!A1:F5';
+const range='Data!A1:F100';
 const apiKey='AIzaSyBb1-sH8j-c6qSKNT4UK7CqP65w7v-ugq8';
 const urlOriginal=`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetsId}/values/${range}?key=${apiKey}`;
 let dataValue;
@@ -12,6 +12,7 @@ function initialize(url){
 	.then(data =>{
 		console.log("data",data);
 		dataValue=transformData(data);
+		getOptionCategory(dataValue);
 	})
 	.catch(error=> console.log("error:*",error))
 	
@@ -31,7 +32,7 @@ function transformData(data){
 			arrays.push(objetoPersonal);
 		}
 	}
-	//console.log("arraysConvertidos",arrays)
+	console.log("arraysConvertidos",arrays)
 	return arrays;
 }
 
@@ -43,9 +44,41 @@ function valor(value){
 	//console.log("valoresdePropiedades",value)
 	return value;
 }
+//continuacion de mi codigo html
 
-  //continuacion de mi codigo html
+//separador por categorias 
+function getOptionCategory(arrObj){
+	let tlapaleria=arrObj.filter(objeto => objeto.category == 'Tlapaleria general');
+	let plomeria=arrObj.filter(objeto => objeto.category == 'Plomeria');
+	let herreria=arrObj.filter(objeto => objeto.category == 'Herreria');
+	let cartones=arrObj.filter(objeto => objeto.category == 'Cartones');
+	let empaques=arrObj.filter(objeto => objeto.category == 'Empeques');
+	let categories={
+		tlapaleria:tlapaleria,
+		plomeria:plomeria,
+		herreria:herreria,
+		cartones:cartones,
+		empaques:empaques
+	}
+	setEventOptions(categories);
+	return categories;
+}
+//integrando for in de objeto para selector 
+function setEventOptions(categoriesObj){
+	for(const property in categoriesObj){
+		let selectContainer=document.querySelector(`#${property}Content`);
+		let optionSelect=categoriesObj[property].map(infoProduct => `<option>${infoProduct.name}</option>`);
+	    //optionSelect.unshift(`<option disabled selected>${categoriesObj.property[0].category}</option>`);
+		selectContainer.innerHTML=optionSelect;
+		selectContainer.addEventListener('change',(e)=> selectOption(e),false);
+	}
+}
+//console por si index y valor
+function selectOption(event) {
+    console.log('seleccionada:::', event.target.value, 'index: ', event.target.selectedIndex);
+  }
 
+//input de html + eventos 
 let input=document.getElementById('searchInput');
 let lista=document.getElementById('list');
 input.addEventListener('keyup',()=> typing(dataValue))
@@ -64,16 +97,18 @@ function typing(dataMyList){
     productsList= result.map((product,index) =>`<li id="${index}" class="item">${product.charAt(0).toUpperCase()+ product.slice(1)}</li>`).join("");
     
     lista.innerHTML =(productsList.length != 0) ? productsList:"No se encontro coincidencias";
+    //console.log("productsList",productsList);
     setEventList();
     return productsList;
   }
 }
 
+//eventos de las lista de resultados del input
 function setEventList(){
 	let getLi=Array.from(document.getElementsByClassName("item"));
 	getLi.forEach((elemento) => elemento.addEventListener("click",()=>selectItem(elemento),false));
 }
-
+//funcion que agrega a funcion  typing para entrar al nombre del producto
 function getProductList(valuesArr){
 
 	let names=valuesArr.map(objeto => objeto.name.toLowerCase())
@@ -103,10 +138,10 @@ let nuevoAbc=abecedario.map(elemento =>`<li class="abc">${elemento}</li>`).join(
 ul.innerHTML=nuevoAbc;
 
 //inicializacion de carrucel desde materialize
- document.addEventListener('DOMContentLoaded', function() {
+ /*document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, options);
-  });
+  });*/
  
 
   // Or with jQuery
@@ -117,15 +152,14 @@ ul.innerHTML=nuevoAbc;
 
   //inicializacions de modal
 
-  document.addEventListener('DOMContentLoaded', function() {
+  /*document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems, options);
-  });
+  });*/
 
   // Or with jQuery
 
-  $(document).ready(function(){
+ $(document).ready(function(){
     $('.modal').modal();
   });
-
 
