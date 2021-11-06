626febecfe74d8de6476aa089ad466e2fb36b14c
Amelia GA <ameliaGA11112@gmail.com>
2021-11-05 19:00:11 -0600

    abecedario integrado deacuerdo a los datos

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,9 +1,10 @@
 const spreadsheetsId='1WfxVETEfl-fmU4QGfOG1wk-93ClARjK5wkgUbAg3BXg';
 //const range='Respuestas de formulario 1!A1:F5';
-const range='Data!A1:I150';
+const range='Data!A1:I178';
 const apiKey='AIzaSyBb1-sH8j-c6qSKNT4UK7CqP65w7v-ugq8';
 const urlOriginal=`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetsId}/values/${range}?key=${apiKey}`;
 let dataValue;
+var contadorInicial = 0;
 window.onload=initialize(urlOriginal);
 
 function initialize(url){
@@ -13,6 +14,7 @@ function initialize(url){
 		console.log("data",data);
 		dataValue=transformData(data);
 		getOptionCategory(dataValue);
+		getAbecedario(getProductList(dataValue));
 		getImages(dataValue[7]);
 	})
 	.catch(error=> console.log("error:*",error))
@@ -39,16 +41,77 @@ function transformData(data){
 
 
 function valor(value){
-	if(value.indexOf('*') !== -1){
-		return value.split('*').filter(element => element != "").map(val => val.trim());
-	}
-	if(value.indexOf(',') !== -1){
-		return value.split(',').filter(element => element != "").map(val => val.trim());
+	if(value){ 
+		if(value.indexOf('*') !== -1){
+			return value.split('*').filter(element => element != "").map(val => val.trim());
+		}
+		if(value.indexOf(',') !== -1){
+			return value.split(',').filter(element => element != "").map(val => val.trim());
+		}
 	}
 	//console.log("valoresdePropiedades",value)
 	return value;
 }
 //continuacion de mi codigo html
+
+//input de html + eventos 
+let input=document.getElementById('searchInput');
+let lista=document.getElementById('list');
+input.addEventListener('keyup',()=> typing(dataValue));
+
+
+function typing(dataMyList){
+	let productsArr=getProductList(dataMyList);
+  let productsList='';
+  if(input.value == '' ){
+    //se oculta la lista
+    lista.setAttribute('class','hide')
+  } else{
+    //se muestra lista
+    lista.removeAttribute('class','hide');
+      let result = productsArr.filter((elemento,indice,array)=>  elemento.indexOf(input.value.toLowerCase()) != -1);
+    productsList= result.map((product,index) =>`<li id="${index}" class="item">${product.charAt(0).toUpperCase()+ product.slice(1)}</li>`).join("");
+    
+    lista.innerHTML =(productsList.length != 0) ? productsList:"No se encontro coincidencias";
+    //console.log("productsList",productsList);
+    setEventList();
+    return productsList;
+  }
+}
+
+//eventos de las lista de resultados del input
+function setEventList(){
+	let getLi=Array.from(document.getElementsByClassName("item"));
+	getLi.forEach((elemento) => elemento.addEventListener("click",()=>selectItem(elemento),false));
+}
+//funcion que agrega a funcion  typing para entrar al nombre del producto
+function getProductList(valuesArr){
+
+	let names=valuesArr.map(objeto => objeto.name.toLowerCase())
+	console.log("propiedades",names);
+	return names;
+}
+function getAbecedario(namesArr){
+	// creando abecedario
+	let abecedario="abcdefghijklmnñopqrstuvwxyz";
+	abecedario=abecedario.split('');
+	//busqueda por inicial abecedario
+	let primeraLetra=namesArr.map(name=> name[0]);
+	let letrasEncontradas=abecedario.filter(letra => primeraLetra.indexOf(letra) != -1); 
+	let ul=document.getElementById("abecedario");
+	let nuevoAbc=letrasEncontradas.map(elemento =>`<li class="abc">${elemento}</li>`).join("").toUpperCase();
+	ul.innerHTML=nuevoAbc;	                                        
+console.log("letra encontrada",nuevoAbc);
+}
+
+function selectItem(opcion){
+	console.log("selectItem",opcion);
+}
+
+//carrouzel
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
 
 //separador por categorias 
 function getOptionCategory(arrObj){
@@ -92,15 +155,6 @@ function setEventOptions(categoriesObj){
 function selectOption(event) {
     console.log('seleccionada:::', event.target.value, 'index: ', event.target.selectedIndex);
   }
-//datos de cada producto en la pag de producto 
-function setInfoProduct(infoCadaUno){
-	console.log("infoCadaUno",infoCadaUno);
-	document.getElementById('information').innerHTML=
-	`<div> ${infoCadaUno.name}</div>
-	<div> ${infoCadaUno.size}</div>
-	<div> ${infoCadaUno.price}</div>
-	`
-}
 
 //traer imagenes pagina del producto
 function getImages(productInfo) {
@@ -109,74 +163,43 @@ function getImages(productInfo) {
     let img=productInfo.imagen;
     document.getElementById('mainImg').innerHTML = `<img class="urls" src="${img[0]}"/>`; 
     document.getElementById('listImg').innerHTML = img.map(url => `<img class="urls" src="${url}" />`).join('');
-    console.log("imagen",img);
-    setInfoProduct(productInfo);
+   // console.log("imagen",img);
+    setInfoProduct(productInfo, "information");
+    setEventContador();
+}
+//datos de cada producto en la pag de producto 
+function setInfoProduct(infoCadaUno,nameNodo){
+	console.log("infoCadaUno",infoCadaUno);
+	document.getElementById(nameNodo).innerHTML=
+	`<div> ${infoCadaUno.name}</div>
+	<div> ${infoCadaUno.size}</div>
+	<div> ${infoCadaUno.price}</div>`
+	
 }
 
 //boton mas y menos de la pag producto
-var menos=document.getElementById('rest');
-menos.addEventListener('click',()=>contadormenos());
-                                                            
-var mas=document.getElementById('sum');
-mas.addEventListener('click',()=>contadormas());
+function setEventContador(){ 
+	var menos=document.getElementById('rest');
+	menos.addEventListener('click',()=>contadormenos());
+	                                                            
+	var mas=document.getElementById('sum');
+	mas.addEventListener('click',()=>contadormas());
 
-var valor = document.getElementById("valor");
-
-var empieza = 0; 
+	var contador = document.getElementById("valor");
+}
 function contadormas(){
-    
-    valor.value = empieza;
-    empieza = empieza + 1;
+	contador.value = contadorInicial;
+	contadorInicial = contadorInicial + 1;
 }
 function contadormenos(){ 
-    if(empieza>=1){
-        empieza = empieza - 1; 
-        valor.value = empieza;
+    if(contadorInicial>=1){
+        contadorInicial = contadorInicial - 1; 
+        contador.value = contadorInicial;
     }
 }
 
 
-//input de html + eventos 
-let input=document.getElementById('searchInput');
-let lista=document.getElementById('list');
-input.addEventListener('keyup',()=> typing(dataValue));
 
-
-function typing(dataMyList){
-	let productsArr=getProductList(dataMyList);
-  let productsList='';
-  if(input.value == '' ){
-    //se oculta la lista
-    lista.setAttribute('class','hide')
-  } else{
-    //se muestra lista
-    lista.removeAttribute('class','hide');
-      let result = productsArr.filter((elemento,indice,array)=>  elemento.indexOf(input.value.toLowerCase()) != -1);
-    productsList= result.map((product,index) =>`<li id="${index}" class="item">${product.charAt(0).toUpperCase()+ product.slice(1)}</li>`).join("");
-    
-    lista.innerHTML =(productsList.length != 0) ? productsList:"No se encontro coincidencias";
-    //console.log("productsList",productsList);
-    setEventList();
-    return productsList;
-  }
-}
-
-//eventos de las lista de resultados del input
-function setEventList(){
-	let getLi=Array.from(document.getElementsByClassName("item"));
-	getLi.forEach((elemento) => elemento.addEventListener("click",()=>selectItem(elemento),false));
-}
-//funcion que agrega a funcion  typing para entrar al nombre del producto
-function getProductList(valuesArr){
-
-	let names=valuesArr.map(objeto => objeto.name.toLowerCase())
-	console.log("propiedades",names);
-	return names;
-}
-
-function selectItem(opcion){
-	console.log("selectItem",opcion);
-}
 
 //inicializacion de vento de Icono a las lista por categoria
 //funcion anonima 
@@ -187,13 +210,7 @@ function selectItem(opcion){
 ()=> ""*/
 
 
-// creando abecedario
-let abecedario="abcdefghijklmnñopqrstuvwxyz";
-abecedario=abecedario.split('');
-console.log(abecedario);
-let ul=document.getElementById("abecedario");
-let nuevoAbc=abecedario.map(elemento =>`<li class="abc">${elemento}</li>`).join("").toUpperCase();
-ul.innerHTML=nuevoAbc;
+
 
 //inicializacion de carrucel desde materialize
  /*document.addEventListener('DOMContentLoaded', function() {
@@ -204,9 +221,7 @@ ul.innerHTML=nuevoAbc;
 
   // Or with jQuery
 
-  $(document).ready(function(){
-    $('.carousel').carousel();
-  });
+  
 
   //inicializacions de modal
 
